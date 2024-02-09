291b66e8a617536ddca0728330c05119597b7b4e
Prateek Verma <89057924+prateekverma999@users.noreply.github.com>
2024-02-09 18:49:45 +0530

    cidr conflict error resolve

--- subnettask/main.tf
+++ subnettask/main.tf
@@ -65,7 +65,7 @@ resource "aws_subnet" "pro_public_subnet" {
 resource "aws_subnet" "pro_private_subnet" {
     count = length(var.private_cidr)
     vpc_id = aws_vpc.pro_vpc.id
-    cidr_block = var.public_cidr[count.index]
+    cidr_block = var.private_cidr[count.index]
     availability_zone = data.aws_availability_zones.available.names[count.index]
 
     tags = {
--- subnettask/variable.tf
+++ subnettask/variable.tf
@@ -10,10 +10,10 @@ variable dest_cidr {
 
 variable public_cidr {
     type = list(string)
-    default = ["10.0.1.0/24", "10.0.2.0/24"]
+    default = ["10.0.1.0/24", "10.0.3.0/24"]
 }
 
 variable private_cidr {
     type = list(string)
-    default = ["10.0.3.0/24", "10.0.4.0/24"]
+    default = ["10.0.2.0/24", "10.0.4.0/24"]
 }
